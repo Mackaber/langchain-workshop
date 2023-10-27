4f2d3c61e101feba375c83a37443f303a0dab73f
Mackaber <x@mackaber.me>
2023-10-27 08:42:40 -0600

    active-ipynb

--- st_tutorial_1/utils.py
+++ st_tutorial_1/utils.py
@@ -1,12 +1,30 @@
-# Inspired from [LangChain tutorial #1: Build an LLM-powered app in 18 lines of code
-# ](https://blog.streamlit.io/langchain-tutorial-1-build-an-llm-powered-app-in-18-lines-of-code/)
+# ---
+# jupyter:
+#   jupytext:
+#     custom_cell_magics: kql
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
 
-# !pip install streamlit openai langchain 
+# %% [markdown]
+# Inspired from [LangChain tutorial #1: Build an LLM-powered app in 18 lines of code](https://blog.streamlit.io/langchain-tutorial-1-build-an-llm-powered-app-in-18-lines-of-code/)
 
+# %%
+# %pip install streamlit openai langchain 
+
+# %%
 global openai_api_key
 openai_api_key="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"
 
-# +
+# %%
 from langchain.llms import OpenAI
 
 def generate_response(input_text):
@@ -14,10 +32,10 @@ def generate_response(input_text):
   return llm(input_text)
 
 
-# -
-
+# %%
 prompt = """
     Hola, como estas?
 """
 
-print(generate_response(prompt))
+# %% tags=["active-ipynb"]
+# print(generate_response(prompt))
