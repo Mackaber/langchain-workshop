f174b6061e84400eb8e6a46e708fe752cbe1fe2f
Mackaber Witckin <mkbravo555@gmail.com>
2023-11-05 19:43:46 -0600

    missed a file

--- 1_prompt_answer/app.py
+++ 1_prompt_answer/app.py
@@ -1,15 +1,16 @@
-import playground
+from playground import generate_response
+import playground as pg
 import streamlit as st
 
 st.title('🦜🔗 Tu primera aplicación')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+pg.openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 with st.form('my_form'):
   prompt = st.text_area('Introduce texto:', 'Cuales son las bases para aprender Inteligencia Artificial?')
   submitted = st.form_submit_button('Enviar')
-  if not openai_api_key.startswith('sk-'):
+  if not pg.openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
+  if submitted and pg.openai_api_key.startswith('sk-'):
     response = generate_response(prompt)
     st.info(response)
